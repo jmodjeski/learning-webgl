0f7e956614085862f75381d0c1908e7704729344
Joe Modjeski <joemodjeski@FeenixDust.local>
2016-02-13 22:56:57 -0700

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: "./static/js/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { 
+                test: /\.js$/, 
+                exclude: /(node_modules)/,
+                loader: "babel" 
+            }
+        ]
+    }
+};
